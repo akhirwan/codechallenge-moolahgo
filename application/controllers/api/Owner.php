2da19398d5ec60eb91d761d4ead3cdbf98a30331
Akhirwan Novendi <akhirwan.novendi@gmail.com>
2021-09-02 15:43:47 +0700

    update http code

--- application/controllers/api/Owner.php
+++ application/controllers/api/Owner.php
@@ -6,12 +6,79 @@ class Owner extends REST_Controller {
 
 	public function __construct(){
 
+		header('Access-Control-Allow-Origin: *');
+		header('Access-Control-Allow-Headers: *');
+    	header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE');
+		header('Accept: application/json');
+		header('Content-Type: application/json; charset=utf-8');
+
 	    parent::__construct();
 	    //load database
 	    $this->load->database();
 	    $this->load->model("api/app_model");
 	    $this->load->library(array("form_validation"));
 	    $this->load->helper("security");
+
+	}
+
+	public function index_get() {
+
+		// default variable
+		$response = [];
+		$http_codes = REST_Controller::HTTP_OK;
+
+		$name = "";
+	    $email = "";
+	    $phone = "";
+    	if (isset($_GET['name'])) $name = $_GET['name'];
+    	if (isset($_GET['email'])) $email = $_GET['email'];
+    	if (isset($_GET['phone'])) $phone = $_GET['phone'];
+
+		if(empty($name) && empty($email) && empty($phone)) {
+
+	    	// we have some empty field
+
+	    	// $http_codes = REST_Controller::HTTP_NOT_FOUND;
+
+	    	$response["status"] = 0;
+	    	$response["message"] = "All fields are needed";
+	    	$response["referralcode"] = "";
+
+	    } else {
+
+		    // parameters insert
+		    $code = $this->app_model->generate_code(); // get code with generate random string
+		    $owner = [
+			    "name" 	 		=> $name,
+		        "email"  		=> $email,
+		        "phone" 		=> $phone,
+		        "referralcode" 	=> $code,
+		        "status" 		=> 1,
+		        "created_at" 	=> date('Y-m-d H:i:s'),
+		    ];
+
+			if($this->app_model->post_code_owner($owner)){
+
+				// insert is successed
+				$response["status"] = 1;
+		    	$response["message"] = "New referral code has been created";
+		    	$response["referralcode"] = $code;
+
+			}else{
+
+				// insert is failed
+				// $http_codes = REST_Controller::HTTP_INTERNAL_SERVER_ERROR;
+
+				$response["status"] = 0;
+		    	$response["message"] = "Failed to create new owner code";
+		    	$response["referralcode"] = "";
+
+			}
+
+	    }
+
+		// api response
+		$this->response($response, $http_codes);
 
 	}
 
--- application/controllers/api/Process.php
+++ application/controllers/api/Process.php
@@ -6,12 +6,53 @@ class Process extends REST_Controller {
 
 	public function __construct(){
 
+		header('Access-Control-Allow-Origin: *');
+		header('Access-Control-Allow-Headers: *');
+    	header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE');
+		header('Accept: application/json');
+		header('Content-Type: application/json');
+
 	    parent::__construct();
 	    //load database
 	    $this->load->database();
 	    $this->load->model("api/app_model");
 	    $this->load->library(array("form_validation"));
 	    $this->load->helper("security");
+
+	}
+
+	public function index_get() {
+
+		// default variable
+		$response = [];
+		$http_codes = REST_Controller::HTTP_OK;
+
+		$code = '000000';
+    	if (isset($_GET['referral_code'])) $code = $_GET['referral_code'];
+
+		// call query in model
+		$owner = $this->app_model->get_code_owner($code);
+
+		if (empty($owner)) {
+
+			// undefined referral code
+			// $http_codes = REST_Controller::HTTP_NOT_FOUND;
+
+	    	$response["status"] = 0;
+	    	$response["message"] = "Code not found";
+	    	$response["data"] = $owner;
+
+		} else {
+
+			// found referral code
+			$response["status"] = 1;
+	    	$response["message"] = "Code owner detail is found";
+	    	$response["data"] = $owner;				
+
+		}
+
+		// api response
+		$this->response($response, $http_codes);
 
 	}
 
